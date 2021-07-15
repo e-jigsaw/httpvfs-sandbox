a946d234e2c925c5dca5188650ff6fd8de7611ca
jigsaw <m@jgs.me>
2021-07-16 04:01:10 +0900

    pretty

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,30 @@
-import { createDbWorker } from "sql.js-httpvfs";
+import { createDbWorker } from 'sql.js-httpvfs'
 
 const workerUrl = new URL(
-  "sql.js-httpvfs/dist/sqlite.worker.js",
+  'sql.js-httpvfs/dist/sqlite.worker.js',
   import.meta.url
-);
-const wasmUrl = new URL("sql.js-httpvfs/dist/sql-wasm.wasm", import.meta.url);
+)
+const wasmUrl = new URL('sql.js-httpvfs/dist/sql-wasm.wasm', import.meta.url)
 
 async function load() {
   const worker = await createDbWorker(
     [
       {
-        from: "inline",
+        from: 'inline',
         config: {
-          serverMode: "full",
-          url: "./example.sqlite3",
+          serverMode: 'full',
+          url: './example.sqlite3',
           requestChunkSize: 4096,
         },
       },
     ],
     workerUrl.toString(),
     wasmUrl.toString()
-  );
+  )
 
-  const result = await worker.db.query(`select * from mytable`);
+  const result = await worker.db.query(`select * from mytable`)
 
-  document.body.textContent = JSON.stringify(result);
+  document.body.textContent = JSON.stringify(result)
 }
 
-load();
+load()
